aa3e6c3e57aee8cf13539b25ae6383e8f5d85ca1
Remy Raes <contact@remyraes.com>
2023-05-16 19:15:39 +0200

    feat: add new relic configuration

--- app/src/main/java/tpl/crashreporting/template/MainActivity.java
+++ app/src/main/java/tpl/crashreporting/template/MainActivity.java
@@ -1,7 +1,7 @@
 package tpl.crashreporting.template;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.newrelic.agent.android.NewRelic;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NewRelic.withApplicationToken(
+                "eu01xx09c8d719d87565a3f0e99f6d9009db6a5add-NRMA"
+        ).start(this.getApplicationContext());
     }
 
     public void onButtonClick(android.view.View view) {
